f456fcc877bd05618a7d093c8eac86c97611de46
Cameron Peddle <cameron_pedd04@hotmail.com>
2023-05-20 01:08:48 -0230

    Adding files

---
+++ manager.php
@@ -0,0 +1,25 @@
+<!--The page a manager role sees-->
+<?php 
+include 'header.php';
+$userInfo = $_SESSION['userInfo'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="UTF-8">
+        <style></style>
+        <link rel="stylesheet" href="style2.css" type="text/css">
+    </head>
+    <body>
+        <main>
+            <h1>Hello <?=$userInfo['firstname']?> <?=$userInfo['lastname']?> (Manager)</h1>
+            <a href="lostpassword.php">Forgot Password</a>
+            <a href="notworking.php">My PC isn't working</a>
+        </main>
+    </body>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+    <script src="script.js"></script>
+</html>
+<?php include 'footer.php' ?>
---
+++ newaccount.php
@@ -0,0 +1,33 @@
+<!--The page looking for an email for new account-->
+<?php 
+include 'header.php';
+$userInfo = $_SESSION['userInfo'];
+if(isset($_POST['submit'])) {
+    if($_POST['email'] != "") {
+        $_SESSION['emailType'] = "newaccount"; ?>
+        <script>window.location.href = "emailsent.php";</script>
+    <?php }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta charset="UTF-8">
+        <style></style>
+        <link rel="stylesheet" href="style2.css" type="text/css">
+    </head>
+    <body>
+        <main>
+            <form class="input" method="post" action="newaccount.php">
+                <h2>Please enter the email for the new account:</h2>
+                <label for="email">Enter emial address:</label>
+                <input type="text" id="email" name="email"/>
+                <input class="button" type="submit" name="submit"/>
+        </main>
+    </body>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+    <script src="script.js"></script>
+</html>
+<?php include 'footer.php' ?>
